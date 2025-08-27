8453ab2c30a9c20302009881c1861305cc5d70ef
Cosmic AI <support@cosmicjs.com>
2025-08-26 19:30:20 -0700

    Fix deployment error by replacing styled-jsx with CSS classes in ContentSection

--- components/ContentSection.tsx
+++ components/ContentSection.tsx
@@ -28,16 +28,6 @@ export default function ContentSection({ title, content }: ContentSectionProps) 
           ))}
         </div>
       </div>
-
-      <style jsx global>{`
-        .scrollbar-hide {
-          -ms-overflow-style: none;
-          scrollbar-width: none;
-        }
-        .scrollbar-hide::-webkit-scrollbar {
-          display: none;
-        }
-      `}</style>
     </section>
   )
 }
